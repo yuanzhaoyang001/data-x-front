92b3fcb3e7c9de623468c534246afc354c7fa156
wang <smalljop@gmail.com>
2024-01-16 16:46:11 +0800

    fix: ScanCode.vue

--- src/views/formgen/components/FormItem/TScan/ScanCode.vue
+++ src/views/formgen/components/FormItem/TScan/ScanCode.vue
@@ -222,8 +222,8 @@ export default {
   position: fixed;
   top: 0;
   left: 0;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
   z-index: 9999;
   display: flex;
   justify-content: center;
